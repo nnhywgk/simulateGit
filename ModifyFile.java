a8c29b9238a25c1879d9c389e9310c86ada8eb7d
Harry Sun <17212010071@fudan.edu.cn>
2022-02-09 01:46:51 +0800

    在文件中新增一个方法
    在文件中新增一个方法

--- ModifyFile.java
+++ ModifyFile.java
@@ -2,4 +2,8 @@ public class ModifyFile {
     public static void main(String[] args){
         System.out.println("Hello World!");
     }
+
+    public void ByeWorld(){
+        System.out.println("ByeWorld!");
+    }
 }
