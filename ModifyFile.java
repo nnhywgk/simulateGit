bbbd53ea1689659f02d8dfc10e7b36a89d37f318
Harry Sun <17212010071@fudan.edu.cn>
2022-02-09 01:56:26 +0800

    主分支修改
    主分支修改

--- ModifyFile.java
+++ ModifyFile.java
@@ -2,4 +2,8 @@ public class ModifyFile {
     public static void main(String[] args){
         System.out.println("Bye World!");
     }
+
+    public void mainBranch(){
+        System.out.println("mainBranch");
+    }
 }
