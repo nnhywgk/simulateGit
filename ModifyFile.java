cee28bd1f21e2b97298d8238d7b5843e37783366
Harry Sun <17212010071@fudan.edu.cn>
2022-02-09 01:47:17 +0800

    在文件中删除一个方法
    在文件中删除一个方法

--- ModifyFile.java
+++ ModifyFile.java
@@ -2,8 +2,4 @@ public class ModifyFile {
     public static void main(String[] args){
         System.out.println("Hello World!");
     }
-
-    public void ByeWorld(){
-        System.out.println("ByeWorld!");
-    }
 }
