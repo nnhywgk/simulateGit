d8ae6db579e11cf6e253f1447f3fd56d8bf64780
Harry Sun <17212010071@fudan.edu.cn>
2022-02-09 01:49:41 +0800

    新增一个分支
    新增一个分支

--- ModifyFile.java
+++ ModifyFile.java
@@ -2,4 +2,8 @@ public class ModifyFile {
     public static void main(String[] args){
         System.out.println("Bye World!");
     }
+
+    public void newBranch(){
+        System.out.println("newBranch");
+    }
 }
